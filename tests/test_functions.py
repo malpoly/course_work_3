cd6349f95aa7fcbe5074b6a7cead430cff8f4200
malpoly <marry2691@yandex.ru>
2023-03-01 15:32:48 +0300

    добавлен критерий 'EXECUTED'

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,7 +4,7 @@ import pytest
 
 def test_sorting():
     assert functions.sorting_data([{"id": 441945886,
-             "state": "EXECUTED",
+             "state": "CANCELED",
              "date": "2019-08-26T10:50:58.294041",
              "operationAmount": {"amount": "31957.58",
                                  "currency": {"name": "руб.",
@@ -29,7 +29,15 @@ def test_sorting():
                                               "code": "USD"}},
              "description": "Перевод организации",
              "from": "MasterCard 7158300734726758",
-             "to": "Счет 35383033474447895560"}],-1) == ['2019-08-26T10:50:58.294041']
+             "to": "Счет 35383033474447895560"}],-1) == [{"id": 41428829,
+             "state": "EXECUTED",
+             "date": "2019-07-03T18:35:29.512364",
+             "operationAmount": {"amount": "8221.37",
+                                 "currency": {"name": "USD",
+                                              "code": "USD"}},
+             "description": "Перевод организации",
+             "from": "MasterCard 7158300734726758",
+             "to": "Счет 35383033474447895560"}]
     assert functions.sorting_data([{"id": 441945886,
                                     "state": "EXECUTED",
                                     "date": "2019-08-26T10:50:58.294041",
@@ -57,4 +65,12 @@ def test_sorting():
                                                                      "code": "USD"}},
                                     "description": "Перевод организации",
                                     "from": "MasterCard 7158300734726758",
-                                    "to": "Счет 35383033474447895560"}], -1) == ['2019-08-26T10:50:58.294041']
+                                    "to": "Счет 35383033474447895560"}], -1) == [{"id": 441945886,
+                                    "state": "EXECUTED",
+                                    "date": "2019-08-26T10:50:58.294041",
+                                    "operationAmount": {"amount": "31957.58",
+                                                        "currency": {"name": "руб.",
+                                                                     "code": "RUB"}},
+                                    "description": "Перевод организации",
+                                    "from": "Maestro 1596837868705199",
+                                    "to": "Счет 64686473678894779589"}]
--- utils/functions.py
+++ utils/functions.py
@@ -1,9 +1,10 @@
 import requests
 import json
+from operator import itemgetter
 from datetime import datetime
 
 def load_operations():
-    """Получает иформацию по банковским операциям из какого-то источника"""
+    """Получает иформацию по банковским операциям из источника"""
     operations = requests.get('https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1677696419445&signature=53p_rqALxByUqE2gapbvO79cOlOy7NhzevjwTq3UhKI&downloadName=operations.json')
     all_operations = json.loads(operations.text)
     return all_operations
@@ -11,14 +12,14 @@ def load_operations():
 all_operations = load_operations()
 
 def sorting_data(all_operations, count_operations=-5):
-    """Выводит списком все даты, сортирует их и возвращает пять последних операций"""
+    """Выводит данные по последним 5(по умолчанию) исполненным операциям"""
     data_list =[]
     count = -5 if count_operations is None else count_operations
     for position in all_operations:
-        if position != {}:
-            data_list.append(position['date'])
-    data_list.sort()
-    return data_list[count:]
+        if position != {} and position['state'] == 'EXECUTED':
+            data_list.append(position)
+    new_list = sorted(data_list, key=itemgetter('date'))
+    return new_list[count:]
 
 
 
