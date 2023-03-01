da332905bfb9503b42aefd988e2bfb2363266dd6
malpoly <marry2691@yandex.ru>
2023-03-01 19:19:35 +0300

    добавлены функции print_inference и preparation_accounts

--- utils/functions.py
+++ utils/functions.py
@@ -27,29 +27,34 @@ def change_data(new_list):
         date_new = datetime.fromisoformat(i['date'])
         date_formatted = date_new.strftime("%d.%m.%Y")  # День Месяц Год
         i['date'] = date_formatted
-    return (new_list)
+    return new_list
+
+def preparation_accounts(new_list):
+    """Меняет формат вывода счетов"""
+    for i in new_list:
+        i['to'] = "Счет **" + i['to'][-4:]
+        if i.get(('from')) != None:
+            if "Maestro" in i['from']:
+                i['from'] = "Maestro " + i['from'][8:12] + " **** **** " + i['from'][-4:]
+            elif "Visa" in i['from']:
+                i['from'] = "Visa Classic " + i['from'][12:17] + " **** **** " + i['from'][-4:]
+            elif "Счет" in i['from']:
+                i['from'] = "Счет **" + i['from'][-4:]
+            elif "MasterCard" in i['from']:
+                i['from'] = "MasterCard " + i['from'][11:15] + " **** **** " + i['from'][-4:]
+    return new_list
+
 
 def print_inference(new_list):
     """Подготавливает данные на печать и выводит их"""
     for i in new_list:
-        print(i['date'], i['description'], "" if i.get('from') == None else i.get('from'), "Счет ***", i['to'][-4:],
-          i['operationAmount']['amount'], i['operationAmount']['currency']['name'])
+        print(i['date'], i['description'], "" if i.get('from') == None else i.get('from'), "->", i['to'],
+          i['operationAmount']['amount'], i['operationAmount']['currency']['name'], end="\n\n")
 
 
 all_operations = load_operations()
-new_list = change_data(sorting_data(all_operations))
+new_list = preparation_accounts(change_data(sorting_data(all_operations)))
 print_inference(new_list)
-
-
-
-
-        #if i.get('from') == None:
-            #print("")
-        #else:
-            #print(i.get('from'))
-        #print("" if i.get('from') == None else i.get('from'))
-
-
 
 
 
